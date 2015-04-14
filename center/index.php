674e733f8821c34072ea3ab9f0b00b402fb833a7
Justin Wegner <justinwegner88@gmail.com>
2015-04-14 18:24:54 -0400

    committing version 1 for research nav test

---
+++ center/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Center Navigation</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <link rel="stylesheet" href="../stylesheets/center-main.css">
+
+    </head>
+    <body>
+        
+        <header>
+            <div class="container logo">
+                <p>Example Dealer</p>
+            </div>
+            <div class="container navigation">
+                <?php include "../main-navigation.php"; ?>
+            </div>
+        </header>
+
+        <div class="content-wrapper">
+            <div class="container">
+                <div class="hero-image-wrapper">
+                    <img src="../images/hero-image.jpg" alt="" />
+                </div>
+            </div>  
+        </div>
+
+
+        <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script> -->
+
+        
+    </body>
+</html>
---
+++ main-navigation.php
@@ -0,0 +1,67 @@
+<ul>
+	<li>Home</li>
+	<li class="has-sub-menu">New
+
+	<ul class="sub-menu">
+		<li>New Inventory</li>
+		<li>2015 Model Lineup</li>
+		<li>New Car Specials</li>
+	</ul>
+
+	</li>
+	<li>Used
+
+		<ul class="sub-menu">
+			<li>Used Inventory</li>
+			<li>CPO Inventory</li>
+			<li>Used/CPO Specials</li>
+			<li>CPO Info: Program</li>
+		</ul>
+
+	</li>
+	<li>Finance
+
+		<ul class="sub-menu">
+			<li>Loan Calculator</li>
+			<li>Apply for Financing</li>
+			<li>Credit Prequalify</li>
+			<li>Trade-In Estimator</li>
+			<li>Finance Specials</li>
+		</ul>
+
+	</li>
+	<li>Research
+
+		<ul class="sub-menu">
+			<li>New Vs. Used CPO</li>
+			<li>Used Vs. CPO</li>
+			<li>Purchase Vs. Lease</li>
+			<li>First time Buyer Tips</li>
+			<li>Cost of Ownership</li>
+		</ul>
+
+	</li>
+	<li>Parts &amp; Service
+
+		<ul class="sub-menu">
+			<li>Service Center</li>
+			<li>Schedule Service</li>
+			<li>Order Parts</li>
+			<li>Service Specials</li>
+			<li>OEM Service Info</li>
+			<li>Dealer Service Info</li>
+		</ul>
+
+	</li>
+	<li>About Us
+ 
+		<ul class="sub-menu">
+			<li>Our Company</li>
+			<li>Careers</li>
+			<li>Reviews</li>
+			<li>Our Blog</li>
+			<li>Our Staff</li>
+		</ul>
+
+	</li>
+</ul>
